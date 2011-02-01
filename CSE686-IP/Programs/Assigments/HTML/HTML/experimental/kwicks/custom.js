9a86e8f2e1e0c683c891d9979d4a0d38a302fcee
Ider Zheng <ider.cs@gmail.com>
2011-02-01 01:27:04 +0000

    Modify CSS style, use     background-image: -moz-linear-gradient(0% 22px 90deg, #a62315, #f47321);     background-image: -webkit-gradient(linear, 0% 0%, 0% 70%, from(#f47321), to(#a62315)); to get gradient background
    git-svn-id: http://ider-pc:721/svn/su@16 496343d1-74b1-2849-8041-70ec0ea91c20

---
+++ CSE686-IP/Programs/Assigments/HTML/HTML/experimental/kwicks/custom.js
@@ -0,0 +1,11 @@
+ function my_kwicks(){
+    $('.kwicks').kwicks({
+		duration: 300,   
+        max: 200,  
+        spacing:  0  
+    });
+}  
+
+ $(document).ready(function(){					
+	my_kwicks();
+});
