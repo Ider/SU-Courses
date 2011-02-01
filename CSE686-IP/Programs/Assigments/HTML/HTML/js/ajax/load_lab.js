40bafd0dc7fd1d23861feec8ae33a292841f46f1
Ider Zheng <ider.cs@gmail.com>
2011-02-01 20:20:36 +0000

    Complete ajax load lab table functionality
    git-svn-id: http://ider-pc:721/svn/su@21 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE686-IP/Programs/Assigments/HTML/HTML/js/ajax/load_lab.js
+++ CSE686-IP/Programs/Assigments/HTML/HTML/js/ajax/load_lab.js
@@ -1,1 +1,111 @@
-﻿
+﻿var xmlhttp;
+var container;
+var cssclass;
+
+function loadXMLLab(div, style) {
+
+    container = div;
+    cssclass = style ? style : "";
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = presentLab;
+    xmlhttp.open("GET", "lab.xml", true);
+    xmlhttp.send();
+}
+
+function presentLab() {
+    var txt, x, xx, i;
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        txt = "<table width=\"100%\" border=\"0\" align=\"left\" cellpadding=\"5\" "
+    + "cellspacing=\"0\" class=\"" + cssclass + "\">";
+
+        txt = txt + "<thead> <tr><td>Requirements</td><td> Pages</td><td>Scores</td></tr></thead>"
+        txt = txt + "<tbody><tr>";
+
+        txt = txt + "<td>" + getRequirmentList() + "</td>"
+        txt = txt + "<td>" + getPageList() + "</td>"
+        txt = txt + "<td>" + getScoreList() + "</td>"
+
+        txt = txt + "</tr></table>";
+        document.getElementById(container).innerHTML = txt;
+    }
+}
+
+
+function getRequirmentList() {
+    var list, i;
+    var text, url, link;
+    var x = xmlhttp.responseXML.documentElement.getElementsByTagName("requirement");
+    link = "http:\/\/www.ecs.syr.edu/faculty/fawcett/handouts/CSE686/";
+    list = "<ul>";
+
+    for (i = 0; i < x.length; i++) {
+
+        try {
+            url = x[i].getElementsByTagName("url")[0].firstChild.nodeValue;
+            text = x[i].getElementsByTagName("text")[0].firstChild.nodeValue;
+        }
+        catch (er) {
+            url = "#";
+            text = "";
+        }
+
+        list = list + "<li><a href='" + link + url + "'>" + text + "</a></li>"
+    }
+    list = list + "</ul>"
+
+    return list;
+}
+
+function getPageList() {
+
+    var list, i;
+    var text, url;
+    var x = xmlhttp.responseXML.documentElement.getElementsByTagName("page");
+    list = "<ul style='list-style-type: decimal;'>";
+
+    for (i = 0; i < x.length; i++) {
+
+        try {
+            url = x[i].getElementsByTagName("url")[0].firstChild.nodeValue;
+            text = x[i].getElementsByTagName("text")[0].firstChild.nodeValue;
+
+            list = list + "<li><a href='" + url + "'>" + text + "</a></li>"
+        }
+        catch (er) {
+            url = "#";
+            text = "<span style='color:gray'>Under Construction...</span>";
+
+            list = list + "<li>" + text + "</li>";
+        }
+    }
+    list = list + "</ul>"
+    return list;
+}
+
+function getScoreList() {
+
+    var list, i;
+    var text;
+    var x = xmlhttp.responseXML.documentElement.getElementsByTagName("score");
+    list = "<ul>";
+
+    for (i = 0; i < x.length; i++) {
+
+        try {
+            text = x[i].firstChild.nodeValue;
+        }
+        catch (er) {
+            text = "<span style='color:gray'>Ungraged</span>";
+        }
+
+        list = list + "<li>" + text + "</a></li>"
+    }
+    list = list + "</ul>"
+
+    return list;
+}
