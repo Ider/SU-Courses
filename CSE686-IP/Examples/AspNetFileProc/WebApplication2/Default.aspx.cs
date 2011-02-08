62251a7c97752b39559c662dd474b6185fda8fa8
Ider Zheng <ider.cs@gmail.com>
2011-02-08 22:52:35 +0000

    Convert to VS2010 version
    git-svn-id: http://ider-pc:721/svn/su@40 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE686-IP/Examples/AspNetFileProc/WebApplication2/Default.aspx.cs
+++ CSE686-IP/Examples/AspNetFileProc/WebApplication2/Default.aspx.cs
@@ -14,61 +14,61 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication2
 {
-  public partial class _Default : System.Web.UI.Page
-  {
-    private void FillListBox()
+    public partial class _Default : System.Web.UI.Page
     {
-      string[] dirs = Directory.GetDirectories(TextBox1.Text);
-      ListBox1.Items.Clear();
-      ListBox1.Items.Add("<> ..");
-      foreach (string dir in dirs)
-      {
-        string name = "<> " + Path.GetFileName(dir);
-        ListBox1.Items.Add(name);
-      }
-      string[] files = Directory.GetFiles(TextBox1.Text);
-      foreach (string file in files)
-      {
-        string name = Path.GetFileName(file);
-        ListBox1.Items.Add(name);
-      }
+        private void FillListBox()
+        {
+            string[] dirs = Directory.GetDirectories(TextBox1.Text);
+            ListBox1.Items.Clear();
+            ListBox1.Items.Add("<> ..");
+            foreach (string dir in dirs)
+            {
+                string name = "<> " + Path.GetFileName(dir);
+                ListBox1.Items.Add(name);
+            }
+            string[] files = Directory.GetFiles(TextBox1.Text);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                ListBox1.Items.Add(name);
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session.IsNewSession)
+                Session["currentPath"] = Server.MapPath(".");
+            TextBox1.Text = Session["currentPath"] as string;
+            Directory.SetCurrentDirectory(TextBox1.Text);
+            FillListBox();
+        }
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bool IsDir = false;
+            string name = Request.Form[ListBox1.UniqueID];
+            string temp;
+            if (name[0] == '<')
+            {
+                int pos = name.LastIndexOf('>') + 2;
+                temp = name.Substring(pos);
+                IsDir = true;
+            }
+            else
+                temp = name;
+            if (IsDir)
+            {
+                string debug = Directory.GetCurrentDirectory();
+                string path = Path.GetFullPath(temp);
+                Directory.SetCurrentDirectory(path);
+                Session["currentPath"] = path;
+                TextBox1.Text = path;
+                FillListBox();
+                TextBox2.Text = "";
+            }
+            else
+            {
+                StreamReader sr = new StreamReader(temp);
+                TextBox2.Text = sr.ReadToEnd();
+            }
+        }
     }
-    protected void Page_Load(object sender, EventArgs e)
-    {
-      if (Session.IsNewSession)
-        Session["currentPath"] = Server.MapPath(".");
-      TextBox1.Text = Session["currentPath"] as string;
-      Directory.SetCurrentDirectory(TextBox1.Text);
-      FillListBox();
-    }
-    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-      bool IsDir = false;
-      string name = Request.Form[ListBox1.UniqueID];
-      string temp;
-      if (name[0] == '<')
-      {
-        int pos = name.LastIndexOf('>') + 2;
-        temp = name.Substring(pos);
-        IsDir = true;
-      }
-      else
-        temp = name;
-      if (IsDir)
-      {
-        string debug = Directory.GetCurrentDirectory();
-        string path = Path.GetFullPath(temp);
-        Directory.SetCurrentDirectory(path);
-        Session["currentPath"] = path;
-        TextBox1.Text = path;
-        FillListBox();
-        TextBox2.Text = "";
-      }
-      else
-      {
-        StreamReader sr = new StreamReader(temp);
-        TextBox2.Text = sr.ReadToEnd();
-      }
-    }
-  }
 }
