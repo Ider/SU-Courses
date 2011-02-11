e16c7f19194b41259507fc6780c502544d5f6ec5
Ider Zheng <ider.cs@gmail.com>
2011-02-11 04:55:42 +0000

    change file name
    git-svn-id: http://ider-pc:721/svn/su@51 496343d1-74b1-2849-8041-70ec0ea91c20

---
+++ CodeAnalysis/CodeAnalysis/iderRulesAndAction.h
@@ -0,0 +1,49 @@
+#ifndef IDER_RULES_AND_ACTIONS_H
+#define IDER_RULES_AND_ACTIONS_H
+
+#include "Parser.h"
+#include "functionInfo.h"
+#include <vector>
+
+
+//////////////////////////////////////////////////////////////////////////
+// Rule for function analysis
+//
+// whenever the last token in collection is "{" or "}",do actions,
+// as they are scope separator.
+class FunctionAnalysisRule:public IRule
+{
+public:
+	bool doTest(ITokCollection* pTc)
+	{
+		std::string& endWith = (*pTc)[pTc->length()-1];
+		if (endWith == "{" || endWith == "}")
+		{
+			doActions(pTc);
+			return true;
+		}
+
+		return false;
+	}
+};
+
+//////////////////////////////////////////////////////////////////////////
+// Action for function analysis
+//
+class FunctionAnalysisAction:public IAction
+{
+public:
+	FunctionAnalysisAction(){ curFuncInfo=NULL;}
+	void doAction(ITokCollection* pTc);
+	void showAnalysisResult();
+private:
+	bool isSpecialKeyWord(const std::string& tok);
+    std::string getFunctionName(ITokCollection* pTc);
+	void braceBeginAction(ITokCollection* pTc);
+	void braceEndAction(ITokCollection* pTc);
+	std::vector<funcInfo*> funcInfos;
+	funcInfo* curFuncInfo;
+};
+
+
+#endif
