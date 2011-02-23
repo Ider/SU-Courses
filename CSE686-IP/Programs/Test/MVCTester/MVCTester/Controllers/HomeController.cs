c543ed303ae472a1c98bfad7cc990b17574b95ec
Ider Zheng <ider.cs@gmail.com>
2011-02-23 01:27:47 +0000

    Add MVC Tester Project
    git-svn-id: http://ider-pc:721/svn/su@89 496343d1-74b1-2849-8041-70ec0ea91c20

---
+++ CSE686-IP/Programs/Test/MVCTester/MVCTester/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTester.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Message"] = "Here is about page!";
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            ViewData["Message"] = "Here is main page!";
+            return View();
+        }
+    }
+}
---
+++ CSE686-IP/Programs/Test/MVCTester/MVCTester/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCTester.MvcApplication" Language="C#" %>
