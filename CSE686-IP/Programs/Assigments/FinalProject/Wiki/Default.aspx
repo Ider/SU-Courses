fe0a01dd86f2b5f1ba3c145a4f024c8c039caa50
Ider Zheng <ider.cs@gmail.com>
2011-03-02 04:41:14 +0000

    Add Wiki Project
    git-svn-id: http://ider-pc:721/svn/su@100 496343d1-74b1-2849-8041-70ec0ea91c20

---
+++ CSE686-IP/Programs/Assigments/FinalProject/Wiki/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="Wiki._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Welcome to Wiki
+    </h2>
+    <div style="height: 400px; overflow-y:auto; word-wrap: break-word; word-break: normal;">
+        <asp:Label ID="lblWiki" runat="server" Text="" Height="300px" Width="100%"></asp:Label>
+        <asp:TextBox ID="txtWiki" runat="server" TextMode="MultiLine" Height="300px" Width="97%"></asp:TextBox>
+    </div>
+    <asp:Button ID="btnSumbit" runat="server" Text="Edit" OnClick="btnSumbit_Click" Width="120px" />
+    &nbsp;
+    <asp:Button ID="btnCancel" runat="server" Text="Cancel" Width="120px" OnClick="btnCancel_Click" />
+</asp:Content>
---
+++ CSE686-IP/Programs/Assigments/FinalProject/Wiki/Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Wiki
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ToggleVisiblity(true);
+                GetWikiContent();
+            }
+        }
+
+        private string HTMLEncode(string text)
+        {
+            return Server.HtmlEncode(text).Replace("\n", "<br>").Replace(" ", "&nbsp;");
+        }
+
+        private void GetWikiContent()
+        {
+            string FILE_NAME = Server.MapPath("App_Data\\WikiContent.txt");
+            using (StreamReader sr = File.OpenText(FILE_NAME))
+            {
+                txtWiki.Text = sr.ReadToEnd();
+                lblWiki.Text = HTMLEncode(txtWiki.Text);
+                sr.Close();
+            }
+        }
+
+        private void WriteWikiContent()
+        {
+            string FILE_NAME = Server.MapPath("App_Data\\WikiContent.txt");
+
+            using (StreamWriter sw = File.CreateText(FILE_NAME))
+            {
+                sw.Write(txtWiki.Text);
+                sw.Close();
+
+            }
+        }
+
+        protected void btnSumbit_Click(object sender, EventArgs e)
+        {
+            if (lblWiki.Visible)
+            {
+                ToggleVisiblity(false);
+            }
+            else
+            {
+                lblWiki.Text = HTMLEncode(txtWiki.Text);
+                WriteWikiContent();
+                ToggleVisiblity(true);
+            }
+        }
+
+        private void ToggleVisiblity(bool visible)
+        {
+            lblWiki.Visible = visible;
+            txtWiki.Visible = !visible;
+            btnSumbit.Text = visible ? "Edit" : "Save";
+            btnCancel.Visible = !visible;
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            ToggleVisiblity(true);
+        }
+    }
+}
