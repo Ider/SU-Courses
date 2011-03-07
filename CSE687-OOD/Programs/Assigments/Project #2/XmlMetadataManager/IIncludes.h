e78f01756550b69f175b49dbe5b498a65dbc17c8
Ider Zheng <ider.cs@gmail.com>
2011-03-07 22:29:39 +0000

    change argument of attach method in IInclude class from "path" to "name"
    git-svn-id: http://ider-pc:721/svn/su@119 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE687-OOD/Programs/Assigments/Project #2/XmlMetadataManager/IIncludes.h
+++ CSE687-OOD/Programs/Assigments/Project #2/XmlMetadataManager/IIncludes.h
@@ -8,7 +8,7 @@ class IInclude
 {
 public:
 	virtual ~IInclude(){}
-	virtual bool Attach(std::string path) =0;
+	virtual bool Attach(std::string name) =0;
 	virtual bool Next()=0;
 	virtual std::string GetFullName()=0;
 	virtual std::string GetPackageName()=0;
