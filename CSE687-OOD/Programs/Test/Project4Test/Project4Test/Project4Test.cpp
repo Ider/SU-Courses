523513821651f6e27449f58c65012da3ce04a157
Ider Zheng <ider.cs@gmail.com>
2011-03-08 06:44:15 +0000

    File stream in/out test
    git-svn-id: http://ider-pc:721/svn/su@126 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE687-OOD/Programs/Test/Project4Test/Project4Test/Project4Test.cpp
+++ CSE687-OOD/Programs/Test/Project4Test/Project4Test/Project4Test.cpp
@@ -9,6 +9,10 @@
 #include <stack>
 #include <list>
 #include <vector>
+
+#include <fstream>
+#include <sstream>
+
 #include "str.h"
 using namespace std;
 
@@ -44,34 +48,82 @@ public:
 typedef bool (is_odd::*MemberPre)(const int&); 
 
 
-int main ()
-{
-	int myints[]= {15,36,7,17,20,39,4,1};
-	list<int> mylist (myints,myints+8);   // 15 36 7 17 20 39 4 1
-
-	mylist.remove_if <MyPredicate>(single_digit);      // 15 36 17 20 39
-
-	is_odd o;
-	//mylist.remove_if(o);//you can also use mylist.remove_if<is_odd>(o);
-	
-	//above two lines is equal to flowing line
-	//mylist.remove_if (is_odd());          // 36 20
-
-	mylist.remove_if<MyPredicate>(is_odd::not_odd);
-
-	MyPredicate pre = is_odd::not_odd;
-	//MyPredicate pre2 = o.Is_Odd; //assign 
-
-
-	// pointer to member function
-	MemberPre mp = &is_odd::Is_Odd;
-	(o.*mp)(9);
+// int main ()
+// {
+// 	int myints[]= {15,36,7,17,20,39,4,1};
+// 	list<int> mylist (myints,myints+8);   // 15 36 7 17 20 39 4 1
 // 
-// 	cout << "mylist contains:";
-// 	for (list<int>::iterator it=mylist.begin(); it!=mylist.end(); ++it)
-// 		cout << " " << *it;
+// 	mylist.remove_if <MyPredicate>(single_digit);      // 15 36 17 20 39
+// 
+// 	is_odd o;
+// 	//mylist.remove_if(o);//you can also use mylist.remove_if<is_odd>(o);
 // 	
-	printVector(mylist);
-	return 0;
-}
+// 	//above two lines is equal to flowing line
+// 	//mylist.remove_if (is_odd());          // 36 20
+// 
+// 	mylist.remove_if<MyPredicate>(is_odd::not_odd);
+// 
+// 	MyPredicate pre = is_odd::not_odd;
+// 	//MyPredicate pre2 = o.Is_Odd; //assign 
+// 
+// 
+// 	// pointer to member function
+// 	MemberPre mp = &is_odd::Is_Odd;
+// 	(o.*mp)(9);
+// // 
+// // 	cout << "mylist contains:";
+// // 	for (list<int>::iterator it=mylist.begin(); it!=mylist.end(); ++it)
+// // 		cout << " " << *it;
+// // 	
+// 	printVector(mylist);
+// 	return 0;
+// }
 
+
+
+int main()
+{
+	ifstream* inf = new ifstream();
+	inf->clear();
+	inf->open("Readme.txt");
+
+	ofstream* outf = new ofstream();
+	outf->open("Hello.xml");
+
+	char line[255];
+	if (inf->good())
+	{
+
+		inf->getline(line,255);
+		cout<< line<<endl;
+	}
+	inf->clear();
+	inf->close();
+	inf->open("STR.cpp");
+
+	if (inf->good())
+	{
+		while (!inf->eof())
+		{
+
+			inf->getline(line,255);
+			cout<< line<<endl;
+			if (outf->good())
+			{
+				(*outf)<<line<<endl;
+			}
+		}
+	}
+	else
+	{
+		cout<<"Can't open the file"<<endl;
+		(*outf)<<"Can't open the file"<<endl;
+	}
+
+	inf->close();
+	outf->close();
+
+	delete inf;
+	delete outf;
+
+}
