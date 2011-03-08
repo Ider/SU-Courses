140fe1568cbbba3cfe7b136503b72eb390086139
Ider Zheng <ider.cs@gmail.com>
2011-03-08 07:40:32 +0000

    Complete writing metadata to xml files functionalities of MetaManager Add test stub.
    git-svn-id: http://ider-pc:721/svn/su@130 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE687-OOD/Programs/Assigments/Project #2/XmlMetadataManager/MetaManager.cpp
+++ CSE687-OOD/Programs/Assigments/Project #2/XmlMetadataManager/MetaManager.cpp
@@ -2,21 +2,56 @@
 
 
 #include "MetaManager.h"
-#include "Includes.h"
 
 
+void MetaManager::Build(int argc, char** argv)
+{
+	packMaker.BuildPakcages(argc,argv);
+}
+MetaManager::~MetaManager()
+{
+	packMaker.ClearResults();
 
+	delete gen;
+	delete inc;
+	if (inStream!= &(std::cin))delete inStream;
+	if (outStream != &(std::cout))delete outStream;
+}
 
+void MetaManager::CreateMetaXML()
+{
+	PackIterator pIt;
+	for (pIt = results.begin(); pIt!=results.end(); ++pIt)
+	{
+		PackageInfo& pack = *(pIt->second);
+		OpenOutStream(pack.Name());
+		(*outStream)<<gen->GetMetadata(pack)<<std::endl<<std::endl;
+		CloseOutStream();
+	}
+}
 
+void MetaManager::OpenOutStream(std::string& name)
+{
+	std::ofstream* pFs = dynamic_cast<std::ofstream*> (outStream);
+	if (pFs)
+		pFs->open(xmlFolder+name+".xml");
+}
 
-
+void MetaManager::CloseOutStream()
+{
+	std::ofstream* pFs = dynamic_cast<std::ofstream*> (outStream);
+	if (pFs && pFs->is_open())
+		pFs->close();
+}
 
 #ifdef TEST_META_MANAGER
 
-int main(int argc, char** argv)
+void main(int argc, char** argv)
 {
-
-	return 0;
+	MetaManager mgr2xml;
+	mgr2xml.Build(argc,argv);
+	mgr2xml.CreateMetaXML();
+	std::cout<<std::endl<<"Xml writing complete"<<std::endl;
 }
 
 #endif
--- CSE687-OOD/Programs/Assigments/Project #2/XmlMetadataManager/MetaManager.h
+++ CSE687-OOD/Programs/Assigments/Project #2/XmlMetadataManager/MetaManager.h
@@ -2,31 +2,47 @@
 #define META_MANAGER_H
 
 #include <iostream>
+#include <fstream>
 #include <map>
+#include <direct.h>
 
 #include "IIncludes.h"
 #include "MetaGenerator.h"
 #include "PackageInfo.h"
+#include "Includes.h"
 
 
 class MetaManager
 {
 public:
-	MetaManager(IInclude* iInc, IMetaGenerator* metaGen, std::iostream* stream);
+	//MetaManager():inc(0),gen(0),inStream(0),xmlFolder("MetaXML"){}
+	MetaManager(std::ostream* oS= (new std::ofstream())
+		, std::istream* iS = (new std::ifstream()));
+	~MetaManager();
 	void Build(int argc, char** argv);
+	void CreateMetaXML();
 private:
+	//void Build();
+	void OpenOutStream(std::string& name);
+	void CloseOutStream();
 	IInclude* inc;
 	IMetaGenerator* gen;
 	std::istream* inStream;
+	std::ostream* outStream;
 	PackagesGenerator packMaker;
 	PackMap results;
-
+	const std::string xmlFolder;
 };
 
 
-inline MetaManager::MetaManager(IInclude* iInc, IMetaGenerator* metaGen, std::iostream* stream)
-:inc(iInc),gen(metaGen),inStream(stream)
+inline MetaManager::MetaManager(std::ostream* oS, std::istream* iS)
+:outStream(oS),inStream(iS),xmlFolder("MetaXML\\")
 {
+	inc = new Includes(inStream);
+	gen = new MetaGenerator(inc);
+	packMaker.SetResultContainer(results);
+	
+	mkdir(xmlFolder.c_str());
 }
 
 #endif
