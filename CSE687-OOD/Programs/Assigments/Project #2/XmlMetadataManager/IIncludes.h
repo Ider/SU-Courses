9b78a6fed844d9c42cf5b212879ee144a48b02fb
Ider Zheng <ider.cs@gmail.com>
2011-03-08 00:51:26 +0000

    Add comments
    git-svn-id: http://ider-pc:721/svn/su@123 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE687-OOD/Programs/Assigments/Project #2/XmlMetadataManager/IIncludes.h
+++ CSE687-OOD/Programs/Assigments/Project #2/XmlMetadataManager/IIncludes.h
@@ -1,5 +1,14 @@
 #ifndef IINCLUDE_H
 #define IINCLUDE_H
+/////////////////////////////////////////////////////////////////////////
+//  IInclude.h    -  IInclude interface						   		   //
+//  ver 1.0                                                            //
+//  Language:       Visual C++, ver 6.0                                //
+//  Platform:       MacBook Pro, Windows7 Pro				           //
+//  Application:    CSE687 project #2	                               //
+//  Author:         Ider Zheng, Syracuse University					   //
+//                  (315) 560-4977, ider.cs@gmail.com				   //
+/////////////////////////////////////////////////////////////////////////
 
 
 #include <string>
