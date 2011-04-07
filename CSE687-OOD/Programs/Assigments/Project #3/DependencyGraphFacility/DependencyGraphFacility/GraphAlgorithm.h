a8f7c5b7845cfebcab51fc2cb56a64d9b768c19e
Ider Zheng <ider.cs@gmail.com>
2011-04-07 11:05:08 +0000

    Add GraphAlgorithm and implement Verteics and Edges search on Graph
    git-svn-id: http://ider-pc:721/svn/su@190 496343d1-74b1-2849-8041-70ec0ea91c20

---
+++ CSE687-OOD/Programs/Assigments/Project #3/DependencyGraphFacility/DependencyGraphFacility/GraphAlgorithm.h
@@ -0,0 +1,97 @@
+#ifndef TEMPLATE_GRAPHALGORITHM_H
+#define TEMPLATE_GRAPHALGORITHM_H
+
+#include "Graph.h"
+
+/************************************************************************/
+/* Graph algorithm helper functors                                      */
+/************************************************************************/
+
+
+template <typename VertexType, typename EdgeType, typename Predicate>
+class VertexFinder
+{
+public:
+	typedef std::list<Vertex<VertexType,EdgeType>*> result_type;
+	//constructors and destructor
+	VertexFinder(Predicate& pr):pred(pr){}
+	VertexFinder(const VertexFinder<VertexType,EdgeType,Predicate>& value)
+		:pred(value.pred),result(value.result){}
+	~VertexFinder(){Clear();}
+
+	void operator()(Vertex<VertexType,EdgeType>& v);
+
+	void Clear(){result.clear();}
+	result_type& Result(){return result;}
+private:
+	const Predicate& pred;
+	result_type result;
+};
+
+template <typename VertexType, typename EdgeType, typename Predicate>
+void VertexFinder<VertexType,EdgeType,Predicate>::operator()(Vertex<VertexType,EdgeType>& v)
+{
+	//when the vertex satisfy the predicate, add its address to result
+	if (pred(v))
+		result.push_back(&v);
+}
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+template <typename VertexType, typename EdgeType, typename Predicate>
+class EdgeFinder
+{
+public:
+	typedef std::list<typename Vertex<VertexType, EdgeType>::pair*> result_type;
+
+	//constructors and destructor
+	EdgeFinder(Predicate& pr):pred(pr){}
+	EdgeFinder(const VertexFinder<VertexType,EdgeType,Predicate>& value)
+		:pred(value.pred),result(value.result){}
+	~EdgeFinder(){Clear();}
+
+	void operator()(Vertex<VertexType,EdgeType>& v);
+
+	void Clear(){result.clear();}
+	result_type& Result(){return result;}
+private:
+	Predicate& pred;
+	result_type result;
+};
+
+template <typename VertexType, typename EdgeType, typename Predicate>
+void EdgeFinder<VertexType,EdgeType,Predicate>::operator()(Vertex<VertexType,EdgeType>& v)
+{
+	for (size_t i=0; i<v.Size(); ++i)
+	{
+		Vertex<VertexType,EdgeType>::pair& edge = v[i];
+		if (pred(edge.first))result.push_back(&edge);
+	}
+}
+
+
+
+
+/************************************************************************/
+/* Graph find algorithm                                                 */
+/************************************************************************/
+
+template <typename VertexType, typename EdgeType, typename Predicate>
+inline typename VertexFinder<VertexType, EdgeType, Predicate>::result_type
+	FindVertics(Graph<VertexType, EdgeType>& graph, Predicate pred)
+{
+	VertexFinder<VertexType, EdgeType, Predicate> finder(pred);
+	graph.DFS(finder);
+	return finder.Result();
+}
+
+template <typename VertexType, typename EdgeType, typename Predicate>
+inline typename EdgeFinder<VertexType, EdgeType,Predicate>::result_type
+	FindEdges(Graph<VertexType, EdgeType>& graph, Predicate pred)
+{
+	EdgeFinder<VertexType, EdgeType, Predicate> finder(pred);
+	graph.DFS(finder);
+	return finder.Result();
+}
+
+#endif
--- CSE687-OOD/Programs/Assigments/Project #3/DependencyGraphFacility/DependencyGraphFacility/TEST.cpp
+++ CSE687-OOD/Programs/Assigments/Project #3/DependencyGraphFacility/DependencyGraphFacility/TEST.cpp
@@ -5,20 +5,57 @@ using namespace std;
 #include <list>
 
 #include "Graph.h"
+#include "GraphAlgorithm.h"
 
-void printInt(Vertex<string,int> v)
+void printInt(const Vertex<string,int>& v)
 {
 	cout<<v.Key()<<"\t edges:";
 		for (size_t i=0 ; i<v.Size();++i)
 		{
 			cout<<v[i].first<<',';
 		}
+	cout <<endl;
+}
+
+bool VertexPredicate(const Vertex<string,int>& v)
+{
+	for (size_t i=0 ; i<v.Size();++i)
+	{
+		if (v[i].first%10==2)
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+bool EdgePredicate(const int e)
+{
+	return e<30;
+}
+
+
+class Printer
+{
+public:
+	void operator()(const Vertex<string,int>& v);
+};
+
+void Printer::operator()(const Vertex<string,int>& v)
+{
+	cout<<v.Key()<<"\t edges:";
+	for (size_t i=0 ; i<v.Size();++i)
+	{
+		cout<<v[i].first<<',';
+	}
 
 	cout <<endl;
 }
 
-void printInt2(Vertex<int,int> v)
-{
+
+
+void printInt2(const Vertex<int,int>& v){
 	cout<<v.Key()<<"\t edges:";
 	for (size_t i=0 ; i<v.Size();++i)
 	{
@@ -28,70 +65,100 @@ void printInt2(Vertex<int,int> v)
 }
 
 void  main () {
-// 	std::pair <std::string,double> product1 ("tomatoes",3.25);
-// 	std::pair <std::string,double*> product2;
-// 	std::pair <std::string,double> product3;
-// 
-// 	double d = 0.99;
-// 	product2.first = "lightbulbs";     // type of first is std::string
-// 	product2.second = &d;            // type of second is double
-// 
-// 	//double* s = (double*)();
-// 
-// 	product3 = std::make_pair ("shoes",20.0);
-// 
-// 	std::cout << "The price of " << product1.first << " is $" << product1.second << "\n";
-// 	std::cout << "The price of " << product2.first << " is $" << product2.second << "\n";
-// 	std::cout << "The price of " << product3.first << " is $" << product3.second << "\n";
-// 	return 0;
+	// 	std::pair <std::string,double> product1 ("tomatoes",3.25);
+	// 	std::pair <std::string,double*> product2;
+	// 	std::pair <std::string,double> product3;
+	// 
+	// 	double d = 0.99;
+	// 	product2.first = "lightbulbs";     // type of first is std::string
+	// 	product2.second = &d;            // type of second is double
+	// 
+	// 	//double* s = (double*)();
+	// 
+	// 	product3 = std::make_pair ("shoes",20.0);
+	// 
+	// 	std::cout << "The price of " << product1.first << " is $" << product1.second << "\n";
+	// 	std::cout << "The price of " << product2.first << " is $" << product2.second << "\n";
+	// 	std::cout << "The price of " << product3.first << " is $" << product3.second << "\n";
+	// 	return 0;
+
 
 	Graph<string,int> text ;
 	try
 	{
 
-	
 		string a ="111";
-	text.AddNode( a);
-	a = "222";
-	text.AddNode(a);
-	a = "333";
-	text.AddNode(a);
-	text.AddNode(string("444"));
+		text.AddNode( a);
+		a = "222";
+		text.AddNode(a);
+		a = "333";
+		text.AddNode(a);
+		text.AddNode(string("444"));
+		text.AddNode(string("555"));
 
-// 	text.AddEdge("111","333",3);
-// 	text.AddEdge("333","222",2);
-// 	text.AddEdge("333","444",4);
-	Graph<string,int> test = text;
-	test = text;
+		// 	text.AddEdge("111","333",3);
+		// 	text.AddEdge("333","222",2);
+		// 	text.AddEdge("333","444",4);
+		Graph<string,int> test = text;
+		test = text;
 
-	vector<pair<string,int>> es;
-	pair<string,int> e;
+		// 	vector<pair<string,int>> es;
+		// 	pair<string,int> e;
+		// 
+		// 	e.first = "444";
+		// 	e.second = 4;
+		// 	es.push_back(e);
+		// 	e.first = "111" ;
+		// 	e.second = 1;
+		// 	es.push_back(e);
+		// 	e.first = "222";
+		// 	e.second = 2;
+		// 	es.push_back(e);
+		// 	text.AddEdge("111",es);
+		// 
+		// 	text.DFS(printInt);
+		test.AddEdge("111","222",12);
+		test.AddEdge("222","333",23);
+		test.AddEdge("333","444",34);
+		test.AddEdge("444","222",42);
+		test.AddEdge("444","555",45);
 
-	e.first = "444";
-	e.second = 4;
-	es.push_back(e);
-	e.first = "111" ;
-	e.second = 1;
-	es.push_back(e);
-	e.first = "222";
-	e.second = 2;
-	es.push_back(e);
-	text.AddEdge("111",es);
 
-	text.DFS(printInt);
-	StrongComponents<string,int> sc(test);
+		test.DFS(Printer());
+		StrongComponents<string,int> sc(test);
+		cout<<"_____________________________________\n";
+		sc.Condensed.DFS(printInt2);
+		cout<<"_____________________________________\n";
 
-	sc.Condensed.DFS(printInt2);
 
-	test.DFS(printInt);
+		test.AddEdge("333","555",35);
+		sc.Rebuild();
+		sc.Condensed.DFS(printInt2);
+std::list<Vertex<string,int>*>
+		x =FindVertics<string,int>(test,VertexPredicate);
 
-	//Graph<string,int>::StrongComponents s = text.GetStrongComponents();
+cout<<endl<<endl<<x.size()<<endl<<endl;
 
-	//s.Condensed.DFS(printInt2);
-	//s.
 
-// 	list<int> b;
-// 	b.remove_if()
+typedef bool (*PRE)(const int);
+	EdgeFinder<string,int,PRE>::result_type r =	FindEdges<string,int>(test,EdgePredicate);
+	
+	cout<<endl<<endl<<r.size()<<endl<<endl;
+	
+	//list<int> ll;
+
+		//ll.push_back(1);
+		//bool h = test==test;
+
+		//test.DFS(printInt);
+
+		//Graph<string,int>::StrongComponents s = text.GetStrongComponents();
+
+		//s.Condensed.DFS(printInt2);
+		//s.
+
+		// 	list<int> b;
+		// 	b.remove_if()
 	}
 	catch(std::string x)
 	{
