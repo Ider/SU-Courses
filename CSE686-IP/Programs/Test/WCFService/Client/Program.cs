b19de29c70b323b0f4cf40b21de71e3eeef69c8f
Ider Zheng <ider.cs@gmail.com>
2011-04-12 20:32:17 +0000

    Add WCF test project. This project is following "Getting Started Tutorial": http://msdn.microsoft.com/en-us/library/ms734712.aspx
    git-svn-id: http://ider-pc:721/svn/su@215 496343d1-74b1-2849-8041-70ec0ea91c20

---
+++ CSE686-IP/Programs/Test/WCFService/Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1: Create an endpoint address and an instance of the WCF Client.
+            CalculatorClient client = new CalculatorClient();
+
+
+            // Step 2: Call the service operations.
+            // Call the Add service operation.
+            double value1 = 100.00D;
+            double value2 = 15.99D;
+            double result = client.Add(value1, value2);
+            Console.WriteLine("Add({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Subtract service operation.
+            value1 = 145.00D;
+            value2 = 76.54D;
+            result = client.Subtract(value1, value2);
+            Console.WriteLine("Subtract({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Multiply service operation.
+            value1 = 9.00D;
+            value2 = 81.25D;
+            result = client.Multiply(value1, value2);
+            Console.WriteLine("Multiply({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Divide service operation.
+            value1 = 22.00D;
+            value2 = 7.00D;
+            result = client.Divide(value1, value2);
+            Console.WriteLine("Divide({0},{1}) = {2}", value1, value2, result);
+
+            //Step 3: Closing the client gracefully closes the connection and cleans up resources.
+            client.Close();
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press <ENTER> to terminate client.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSE686-IP/Programs/Test/WCFService/Service/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/ServiceModelSamples/Service");
+
+            // Step 2 of the hosting procedure: Create ServiceHost
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+
+
+                // Step 3 of the hosting procedure: Add a service endpoint.
+                selfHost.AddServiceEndpoint(
+                    typeof(ICalculator),
+                    new WSHttpBinding(),
+                    "CalculatorService");
+
+
+                // Step 4 of the hosting procedure: Enable metadata exchange.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5 of the hosting procedure: Start (and then stop) the service.
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                // Close the ServiceHostBase to shutdown the service.
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+
+
+        }
+    }
+
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
