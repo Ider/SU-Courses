9448d7dc2cc3154154034dcc50872a83f855b522
Ider Zheng <ider.cs@gmail.com>
2011-04-13 03:28:13 +0000

    Test file upload in MVC at about page
    git-svn-id: http://ider-pc:721/svn/su@219 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE686-IP/Programs/Assigments/FinalProject/FinalProject/Controllers/HomeController.cs
+++ CSE686-IP/Programs/Assigments/FinalProject/FinalProject/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FinalProject.Helper;
+using System.IO;
 
 namespace FinalProject.Controllers
 {
@@ -25,6 +26,23 @@ namespace FinalProject.Controllers
         public ActionResult Layout()
         {
             return View();
+        }
+
+        [HttpPost]
+        public void Upload()
+        {
+            foreach (string inputTagName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[inputTagName];
+                if (file.ContentLength > 0)
+                {
+                    string path = HttpContext.Server.MapPath("../Uploads");
+                    string filePath = Path.Combine(path, Path.GetFileName(file.FileName));
+                    file.SaveAs(filePath);
+                }
+            }
+
+            RedirectToAction("Index");
         }
     }
 }
