9cff6b08cb85072829c46706c0421abfe6a0d274
Ider Zheng <ider.cs@gmail.com>
2011-04-21 23:36:23 +0000

    Add Client and Server projects, seperate client socket and server socket from SocketCommunicator demo into two project. Start up two project at same time
    git-svn-id: http://ider-pc:721/svn/su@233 496343d1-74b1-2849-8041-70ec0ea91c20

---
+++ CSE687-OOD/Programs/Assigments/Project #4/SoftwareRepository/Client/ClientProcess.cpp
@@ -0,0 +1,136 @@
+
+#include "..\Foundation\Communicator.h"
+
+/////////////////////////////////////////////////////////////////////
+// Thread Proc classes for sender
+
+class MsgSender_Proc : public Thread_Processing<MsgSender_Proc>
+{
+public:
+	MsgSender_Proc(IMsgHandler* pMsgHandler) : _pMsgHandler(pMsgHandler) {}
+	void run()
+	{
+		GLock<1> lock;
+		std::string msg;
+		lock.lock();
+		BQueue<std::string>* pBQ = _pMsgHandler->getQueue();
+		ICommunicator* pComm = _pMsgHandler->getCommunicator();
+		EndPoint remoteEp = _pMsgHandler->getEndPoint();
+		lock.unlock();
+		///////////////////////////////////////////////////////
+		// enter your server code here
+		while(true)
+		{
+			sout << locker << "\n  sender received message: " 
+				<< (msg = pBQ->deQ()).c_str() << unlocker;
+			if(msg == "quit")
+				break;
+		}
+		// end of your code
+		///////////////////////////////////////////////////////
+	}
+private:
+	IMsgHandler* _pMsgHandler;
+};
+
+class FileSender_Proc : public Thread_Processing<FileSender_Proc>
+{
+public:
+	FileSender_Proc(IFileHandler* pFileHandler) : _pFileHandler(pFileHandler) {}
+	void run()
+	{
+		GLock<1> lock;
+		std::string msg;
+		lock.lock();
+		BQueue<std::string>* pBQ = _pFileHandler->getQueue();
+		ICommunicator* pComm = _pFileHandler->getCommunicator();
+		EndPoint remoteEp = _pFileHandler->getEndPoint();
+		lock.unlock();
+		///////////////////////////////////////////////////////
+		// enter your server code here
+		while(true)
+		{
+			sout << locker << "\n  sender received file: " 
+				<< (msg = pBQ->deQ()).c_str() << unlocker;
+			if(msg == "quit")
+				break;
+		}
+		// end of your code
+		///////////////////////////////////////////////////////
+	}
+private:
+	IFileHandler* _pFileHandler;
+};
+
+
+void main()
+{
+	sout << locker << "\n  Testing Socket Communicator Client"
+		<< "\n ========================================\n" << unlocker;
+
+	//Application::EnableVisualStyles();
+	//Application::SetCompatibleTextRenderingDefault(false); 
+
+	//// Create the main window and run it
+	// Application::Run(gcnew SocketComm::DemoForm());
+
+	try
+	{
+		EndPoint rep("127.0.0.1",2048);
+		EndPoint sep("127.0.0.1",2049);
+
+		// MsgSender_Proc is your sender's server message handling
+		//   If you don't have sender server file handling don't 
+		//   attach a file handler
+		Communicator sndr(sep);
+		sndr.setFileSource(".\\FilePostTest\\Sender\\");
+		MsgHandler<MsgSender_Proc> smsgHandler;
+		sndr.attachMsgHandler(&smsgHandler);
+		sndr.listen();
+
+		// The MsgReceiver_Proc and FileReceiver_Proc
+		// should connect when they have a reply, so
+		// don't connect the receiver - that's done
+		// by your server code (see above)
+
+		if(!sndr.connect("127.0.0.1",2048))
+		{
+			sout << "\n  failed to connect sndr\n\n";
+			return;
+		}
+
+		sout<<"  start posting\n";
+
+		sndr.postMessage("first message");
+		sndr.postMessage("second message");
+		sndr.postMessage("third and longer message");
+		sndr.postMessage("fourth and still longer and most verbose message");
+		sndr.postFile("test.txt");
+		sndr.postFile("Project #4 Analysis.docx");
+		sndr.postFile("FormWithCppThread.zip");
+		//sndr.postFile("notepad.exe");
+		//sndr.postMessage("quit");
+
+		while(true)
+		{
+			char buf[MAX_PATH];
+			std::cin.getline(buf,MAX_PATH);
+			sndr.postMessage(buf);
+			if (std::string(buf) == "quit")break;
+		}
+
+		sout << locker << "\n  Socket connection count = " 
+			<< sndr.getSocketConnectionCount() << unlocker;
+
+		sout << "\n\n  press key to exit:  ";
+		sout.flush();
+		sout << "\n\n";
+		//    rcvr.disconnect();
+		sndr.disconnect();
+
+	}
+	catch(std::exception& ex)
+	{
+		sout << locker << "\n  " << ex.what() << "\n\n" << unlocker;
+	}
+}
---
+++ CSE687-OOD/Programs/Assigments/Project #4/SoftwareRepository/Repository/ServerProcess.cpp
@@ -0,0 +1,121 @@
+
+#include "..\Foundation\Communicator.h"
+#include <conio.h>
+
+
+class MsgReceiver_Proc : public Thread_Processing<MsgReceiver_Proc>
+{
+public:
+	MsgReceiver_Proc(IMsgHandler* pMsgHandler) : _pMsgHandler(pMsgHandler) {}
+	void run()
+	{
+		GLock<1> lock;
+		std::string msg;
+		lock.lock();
+		BQueue<std::string>* pBQ = _pMsgHandler->getQueue();
+		ICommunicator* pComm = _pMsgHandler->getCommunicator();
+		EndPoint remoteEp = _pMsgHandler->getEndPoint();
+		lock.unlock();
+		///////////////////////////////////////////////////////
+		// insert your server processing here
+		while(true)
+		{
+			sout << locker << "\n  receiver processing message: " 
+				<< (msg = pBQ->deQ()).c_str() << unlocker;
+			lock.lock();
+			EndPoint remoteEp = _pMsgHandler->getEndPoint();
+			if(pComm->connect(remoteEp.getIP(), remoteEp.getPort()))
+			{
+				pComm->postMessage(std::string("got message")+msg+"\n");
+				pComm->disconnect();
+			}
+			else
+				sout << "\n  failed to connect";
+			lock.unlock();
+
+			if(msg == "quit")
+				break;
+		}
+		// end of your code
+		///////////////////////////////////////////////////////
+	}
+private:
+	IMsgHandler* _pMsgHandler;
+};
+
+class FileReceiver_Proc : public Thread_Processing<FileReceiver_Proc>
+{
+public:
+	FileReceiver_Proc(IFileHandler* pFileHandler) : _pFileHandler(pFileHandler) {}
+	void run()
+	{
+
+		GLock<1> lock;
+		std::string msg;
+		lock.lock();
+		BQueue<std::string>* pBQ = _pFileHandler->getQueue();
+		ICommunicator* pComm = _pFileHandler->getCommunicator();
+		EndPoint remoteEp = _pFileHandler->getEndPoint();
+		lock.unlock();
+		///////////////////////////////////////////////////////
+		// insert your server code here
+		while(true)
+		{
+			sout << locker << "\n  receiver processing file: " << (msg = pBQ->deQ()).c_str() << unlocker;
+			lock.lock();
+			EndPoint remoteEp = _pFileHandler->getEndPoint();
+			if(pComm->connect(remoteEp.getIP(), remoteEp.getPort()))
+			{
+				pComm->postMessage(std::string("got file\n"));
+				pComm->disconnect();
+			}
+			else
+				sout << "\n  failed to connect";
+			lock.unlock();
+
+			if(msg == "quit")
+				break;
+		}
+		// end of your code
+		///////////////////////////////////////////////////////
+	}
+private:
+	IFileHandler* _pFileHandler;
+};
+
+void main()
+{
+	sout << locker << "\n  Testing Socket Communicator Server"
+		<< "\n =============================\n" << unlocker;
+
+	//Application::EnableVisualStyles();
+	//Application::SetCompatibleTextRenderingDefault(false); 
+
+	//// Create the main window and run it
+	// Application::Run(gcnew SocketComm::DemoForm());
+
+	try
+	{
+		EndPoint rep("127.0.0.1",2048);
+
+		// MsgReceiver_Proc is your receiver's server message handling
+		// FileReceiver_Proc is your receiver's server file handling
+		Communicator rcvr(rep);
+		MsgHandler<MsgReceiver_Proc> rMsgHandler;
+		rMsgHandler.setCommunicator(&rcvr);
+		rcvr.attachMsgHandler(&rMsgHandler);
+		FileHandler<FileReceiver_Proc> rFileHandler;
+		rFileHandler.setFileDestination(".\\FilePostTest\\Receiver\\");
+		rFileHandler.setCommunicator(&rcvr);
+		rcvr.attachFileHandler(&rFileHandler);
+		rcvr.listen();
+
+
+		_getche();
+
+	}
+	catch(std::exception& ex)
+	{
+		sout << locker << "\n  " << ex.what() << "\n\n" << unlocker;
+	}
+}
