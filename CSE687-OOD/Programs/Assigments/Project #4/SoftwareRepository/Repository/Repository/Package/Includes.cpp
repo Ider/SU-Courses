d07695c72852307e52fec4c33d17b0421ed69276
Ider Zheng <ider.cs@gmail.com>
2011-04-26 16:33:31 +0000

    Upload testfiles
    git-svn-id: http://ider-pc:721/svn/su@266 496343d1-74b1-2849-8041-70ec0ea91c20

---
+++ CSE687-OOD/Programs/Assigments/Project #4/SoftwareRepository/Repository/Repository/Package/Includes.cpp
@@ -0,0 +1,207 @@
+/////////////////////////////////////////////////////////////////////////
+//  Includes.cpp    -  facility to extract include from file   		   //
+//  ver 1.0                                                            //
+//  Language:       Visual C++, ver 6.0                                //
+//  Platform:       MacBook Pro, Windows7 Pro				           //
+//  Application:    CSE687 project #2	                               //
+//  Author:         Ider Zheng, Syracuse University					   //
+//                  (315) 560-4977, ider.cs@gmail.com				   //
+/////////////////////////////////////////////////////////////////////////
+
+
+#include "Includes.h"
+#include <fstream>
+#include <sstream>
+
+//////////////////////////////////////////////////////////////////////////
+//Destructor, close in-stream if it is file stream
+Includes::~Includes()
+{
+	if(inStream)
+	{
+		inStream->clear();
+		std::ifstream* pFs = dynamic_cast<std::ifstream*>(inStream);
+		if(pFs)
+		{
+			pFs->close();
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Open file given by name, if in-stream is file stream
+bool Includes::Attach(std::string name)
+{
+	if(inStream)
+	{
+		inStream->clear();
+	}
+
+	std::ifstream* pFs = dynamic_cast<std::ifstream*>(inStream);
+	if(!pFs)return false;
+
+	pFs->close();
+
+	pFs->open(name);
+	return pFs->good();
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Find next include, if found return true.
+bool Includes::Next()
+{
+	char buffer[bufSize];
+	while(inStream->good())
+	{
+		inStream->getline(buffer,bufSize);
+		if (ExtractInclude(buffer))
+		{
+			ExtractName();
+			return true;
+		}
+	}
+
+	curInc="";
+	curName="";
+	return false;
+}
+//////////////////////////////////////////////////////////////////////////
+//Return full name of package that included
+std::string Includes::GetFullName()
+{
+	return curName;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Remove the path and file extension, return the pure file name
+std::string Includes::GetPackageName()
+{
+	size_t slash = curName.find('\\');
+	size_t dot = curName.find_last_of('.');
+
+	if (slash == std::string::npos)
+		slash = curName.find('/');
+	if (slash == std::string::npos)
+		return curName.substr(0,dot);;
+	
+
+	return curName.substr(slash+1,dot);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Specify whether the include is system or local
+bool Includes::IsSystem()
+{
+	return curInc.find('<')<curInc.length();
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Extract package name from angle bracket
+void Includes::ExtractName()
+{
+	size_t bracket = curInc.find('<');
+	size_t quote = curInc.find('\"');
+	size_t start = std::min(bracket,quote);
+	if (start == std::string::npos)
+	{
+		curName = "";
+		return;
+	}
+
+	bracket = curInc.find_last_of('>');
+	quote = curInc.find_last_of('\"');
+	size_t end = std::min(bracket,quote);
+	if (end ==std::string::npos || end <= start+1)
+	{
+		curName = "";
+		return;
+	}
+
+	curName = curInc.substr(start+1, end-start-1);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Check whether the line get from file is include or not
+bool Includes::ExtractInclude(char* buffer)
+{
+	while(*buffer =='\t'||*buffer ==' ')++buffer;
+	if (*buffer!='#')return false;
+
+	//exclude the pro-process definitions
+	std::string temp (buffer);
+	if (temp.find('<') == std::string::npos 
+		&& temp.find('\"') == std::string::npos )
+	return false;
+
+	curInc = temp;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////< test stub >///////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+
+#ifdef TEST_INCLUDES
+
+void title(const std::string &msg, char underChar='-') {
+
+	std::string over = "\n";
+	over += std::string(msg.size()+2,underChar);
+	std::string under = std::string(msg.size()+2,underChar);
+	std::string body = "\n ";
+	body += msg;
+	body += "\n";
+	body += under;
+	body += '\n';
+	std::cout<< over + body;
+}
+
+
+void main(int argc, char** argv)
+{
+ 	std::string  file = "..\\Includes.cpp";
+	Includes inc(new std::ifstream());
+	title("includes of file: " + file);
+
+	if (inc.Attach(file))
+		while(inc.Next())
+		{
+			std::cout<<(inc.IsSystem()?"System":"Local");
+			std::cout<<"\t\t";
+			std::cout<<inc.GetFullName();
+			std::cout<<"\t\t";
+			std::cout<<inc.GetPackageName();
+			std::cout<<std::endl;
+		}
+		else
+			std::cout<<"Can't open file: " + file;
+
+	if(argc < 2)
+	{
+		std::cout 
+			<< "\n  please enter name of file to process on command line\n\n";
+		return;
+	}
+
+	for(int i=1; i<argc; ++i)
+	{
+		file = argv[i];
+		title("includes of file: " + file);
+
+		if (inc.Attach(file))
+			while(inc.Next())
+			{
+				std::cout<<(inc.IsSystem()?"System":"Local");
+				std::cout<<"\t\t";
+				std::cout<<inc.GetFullName();
+				std::cout<<"\t\t";
+				std::cout<<inc.GetPackageName();
+				std::cout<<std::endl;
+			}
+		else
+			std::cout<<"Can't open file: " + file;
+	}
+}
+
+#endif
---
+++ CSE687-OOD/Programs/Assigments/Project #4/SoftwareRepository/Repository/Repository/Package/MetaGenerator.cpp
@@ -0,0 +1,203 @@
+/////////////////////////////////////////////////////////////////////////
+//  MetaGenerator.cpp    -  facility to generator metadata xml files     //
+//  ver 1.0                                                            //
+//  Language:       Visual C++, ver 6.0                                //
+//  Platform:       MacBook Pro, Windows7 Pro				           //
+//  Application:    CSE687 project #2	                               //
+//  Author:         Ider Zheng, Syracuse University					   //
+//                  (315) 560-4977, ider.cs@gmail.com				   //
+/////////////////////////////////////////////////////////////////////////
+
+#include "MetaGenerator.h"
+
+
+//////////////////////////////////////////////////////////////////////////
+//Get Metadata according from a file that given by the filepath
+std::string MetaGenerator::GetMetadata(std::string& filePath)
+{
+	Clear();
+
+	GeneratePackInfo(filePath);
+	GenerateReferences(filePath);
+
+	return CombineMetaElements(GetKeyName(filePath));
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Get MetaData for a package,retrieve all local reference from the files 
+//that PackageInfo contains
+std::string MetaGenerator::GetMetadata(PackageInfo& pack)
+{
+	Clear();
+
+	for (size_t i =0; i<pack.FileCount(); ++i)
+	{
+		GeneratePackInfo(pack[i]);
+		GenerateReferences(pack[i]);
+	}
+
+	return CombineMetaElements(pack.Name());
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Remove the path and file extension, return the pure file name
+std::string MetaGenerator::GetKeyName(std::string filePath)
+{
+	//remove path
+	//If filePath.find_last_of('\\') failed, it return std::string::npos
+	//npos + 1 equals to 0, so nothing erased
+	filePath.erase(0,filePath.find_last_of('\\')+1);
+	//remove extension
+	size_t pos = filePath.find_last_of('.');
+	if (pos < filePath.size())
+		filePath.erase(pos,filePath.size());
+	return filePath;
+}
+
+//////////////////////////////////////////////////////////////////////////
+//flush all private xml data
+void MetaGenerator::Clear()
+{
+	meta.flush();
+	references.flush();
+	packInfo.flush();
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Generate the basic package information tag
+std::string MetaGenerator::GeneratePackInfo(std::string& filePath)
+{
+	std::string tag;
+	if(filePath[filePath.size()-1] == 'h')
+		tag="head";
+	else
+		tag="implement";
+
+	packInfo.addSibling(xmlElem(tag,filePath));
+
+	return packInfo.format();
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Retrieve all local references from the file
+std::string MetaGenerator::GenerateReferences(std::string& filePath)
+{
+	if(!inc->Attach(filePath))return "";
+
+	while(inc->Next())
+	{
+		if(inc->IsSystem())continue;
+		std::string refTag = "reference name=\""+inc->GetPackageName()+"\"";
+		xmlElem refElem(refTag,inc->GetFullName());
+		references.addSibling(refElem);
+	}
+
+	return references.format();
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Embrace whole <reference> tags with <references> tag
+std::string MetaGenerator::EmbraceReferences()
+{
+	RemoveReferenceEmbrace();
+	references.makeParent(refEnbrace);
+	return references.format();
+}
+
+//////////////////////////////////////////////////////////////////////////
+//Combine packinfo and references tags, and embrace them with Package tag
+//add xml document title
+std::string MetaGenerator::CombineMetaElements(std::string packName)
+{
+	EmbraceReferences();
+
+	std::string attribe = "name=\""+packName+"\"";
+	std::string tag = xmlElem::makeTag("package",attribe);
+	xmlElem	combine(tag,packInfo.xmlStr()+references.xmlStr());
+
+	meta.xmlStr() = combine.elemStr();
+	meta.makeDoc();
+
+	return meta.format();
+}
+
+//////////////////////////////////////////////////////////////////////////
+//If the reference tags is embraced with referEmbrace, remove it
+void MetaGenerator::RemoveReferenceEmbrace()
+{
+	xmlElem refer;
+	if (references.find(refEnbrace,refer))
+		references.xmlStr() = refer.body();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////< test stub >///////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+#ifdef TEST_META_GENERATOR
+
+#include <vector>
+
+class VectorInclude:public IInclude
+{
+public:
+	VectorInclude()
+	{
+		index =-1; 
+		ins.push_back("string");
+		ins.push_back("vector");
+		ins.push_back("list");
+		ins.push_back("iostream");
+		ins.push_back("queue");
+		ins.push_back("stack");
+	}
+	virtual bool Attach(std::string name){index =-1; return true;}
+	virtual bool Next(){return ++index<ins.size();}
+	virtual std::string GetFullName(){return GetPackageName()+".h";}
+	virtual std::string GetPackageName(){return index <ins.size()?ins[index]:"";}
+	virtual bool IsSystem(){return false;};
+private:
+	std::vector<std::string> ins;
+	size_t index;
+};
+//----< create a string with both over and under lines >-----------------
+
+void title(const std::string &msg, char underChar='-') {
+
+	std::string over = "\n";
+	over += std::string(msg.size()+2,underChar);
+	std::string under = std::string(msg.size()+2,underChar);
+	std::string body = "\n ";
+	body += msg;
+	body += "\n";
+	body += under;
+	body += '\n';
+	std::cout<< over + body;
+}
+
+
+void main()
+{
+	MetaGenerator gnrtor(new VectorInclude());
+	std::string fileName = "StandardLibrary.h";
+
+	title("Test Package Name:");
+	std::cout<<fileName<<std::endl;
+
+	title("Test Generate Package information:");
+	std::cout<< gnrtor.GeneratePackInfo(fileName)<<std::endl;
+
+	title("Test Generate References:");
+	std::cout<< gnrtor.GenerateReferences(fileName)<<std::endl;
+	
+	title("Test Embrace References:");
+	std::cout<< gnrtor.EmbraceReferences()<<std::endl;
+
+	title("Test Create Whole Metadata xml:");
+	std::cout<< gnrtor.CombineMetaElements(gnrtor.GetKeyName(fileName))<<std::endl;
+}
+
+
+
+#endif
