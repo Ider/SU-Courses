d179b0a11d8d17726302c92e896e24cabcab8e0f
Ider Zheng <ider.cs@gmail.com>
2011-05-01 20:03:03 +0000

    Set DataDirectory, so that able to connect database cross project
    git-svn-id: http://ider-pc:721/svn/su@329 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE686-IP/Programs/Assigments/FinalProject/Wiki/Global.asax.cs
+++ CSE686-IP/Programs/Assigments/FinalProject/Wiki/Global.asax.cs
@@ -14,6 +14,8 @@ namespace Wiki
         {
             // Code that runs on application startup
 
+            string DataDirectory = @"C:\Users\Ider\Project\SU\CSE686-IP\Programs\Assigments\FinalProject\FinalProject\App_Data";
+            AppDomain.CurrentDomain.SetData("DataDirectory", DataDirectory);
         }
 
         void Application_End(object sender, EventArgs e)
