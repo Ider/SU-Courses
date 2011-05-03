9dd5fca11553752855b5b8ef4256d3caf86d9314
Ider Zheng <ider.cs@gmail.com>
2011-05-03 21:42:12 +0000

    Add appsetting to web.config, add MVC website address. Add menu that navigate to mvc website to master page.
    git-svn-id: http://ider-pc:721/svn/su@334 496343d1-74b1-2849-8041-70ec0ea91c20

--- CSE686-IP/Programs/Assigments/FinalProject/Wiki/Site.Master.cs
+++ CSE686-IP/Programs/Assigments/FinalProject/Wiki/Site.Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
 namespace Wiki
 {
@@ -11,7 +12,8 @@ namespace Wiki
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+                string mvcWebsite = ConfigurationManager.AppSettings["MvcSiteAddress"];
+                NavigationMenu.Items.Add(new MenuItem("MVC") { NavigateUrl = mvcWebsite });
         }
     }
 }
